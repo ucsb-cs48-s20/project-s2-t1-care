096d707b00e68855e76db39200f3359286046a77
Cindy Zhao <cindyzhao126@gmail.com>
2020-06-01 14:30:19 -0700

    updated package-lock.json

---
+++ components/Graph.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Plot from "react-plotly.js";
+function GenerateGraphData(inputList) {
+  let x = [];
+  let y = [];
+  var i;
+  for (i = 0; i < inputList.length; i++) {
+    x.push(i);
+    y.push(parseInt(inputList[i].todaySleep));
+  }
+  let result = {
+    x: x,
+    y: y,
+    type: "scatter",
+  };
+  return result;
+}
+
+class Graph extends React.Component {
+  constructor(props) {
+    super(props);
+    this.user = props.user;
+    this.state = { userData: 0, testList: [], plant2: 0 };
+    this.plant = 10;
+    this.plantIMG = "plantframes/frame_00_delay-0.04s.gif";
+    this.firstLevel = 0;
+  }
+
+  async returnuserData() {
+    const res = await fetch("/api/daily?sub=" + this.user.sub);
+    return res.json();
+  }
+  async componentDidMount() {
+    const userData = await this.returnuserData();
+    if (userData) {
+      console.log(userData.testList);
+      this.firstLevel = userData.testList;
+    }
+    if (userData) {
+      this.setState({ userData: userData, plant2: userData.userData });
+    }
+  }
+
+  render() {
+    let toGraph = [GenerateGraphData(this.firstLevel)];
+    return <Plot data={toGraph} />;
+  }
+}
+export default Graph;
--- pages/history.js
+++ pages/history.js
@@ -1,7 +1,12 @@
 import Layout from "../components/Layout";
+import dynamic from "next/dynamic";
 import JournalForm from "../components/JournalForm";
 import { optionalAuth } from "../utils/ssr";
 import JournalLog from "../components/log";
+//import Graph from "../components/Graph";
+const Graph = dynamic(() => import("../components/Graph"), {
+  ssr: false,
+});
 
 export const getServerSideProps = optionalAuth;
 export default function MultiForm(props) {
@@ -10,6 +15,7 @@ export default function MultiForm(props) {
   return (
     <Layout user={user}>
       <h1> let's see how you're doing! </h1>
+      <Graph user={user}></Graph>
       <JournalLog user={user}></JournalLog>
     </Layout>
   );
