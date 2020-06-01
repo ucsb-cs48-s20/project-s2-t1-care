04d7ed8702ac7bb263c5efa516fb76033e2cb763
Julian Wong <julianwong@ucsb.edu>
2020-05-31 20:19:26 -0700

    unit testing

--- cypress/integration/auth.spec.js
+++ cypress/integration/auth.spec.js
@@ -4,4 +4,16 @@ describe("Authentication", () => {
 
     cy.get("[data-cy=login]").should("exist");
   });
+
+  it("shows me navbar options for unlogged in", () => {
+    cy.visit("http://localhost:3000");
+    cy.get(".navbar-nav").contains("take care of you and your plant");
+    cy.get(".navbar-nav").contains("github");
+  });
+
+  it("shows me navbar options for logged in", () => {
+    cy.visit("http://localhost:3000");
+    cy.get(".navbar-nav").contains("take care of you and your plant");
+    cy.get(".navbar-nav").contains("github");
+  });
 });
