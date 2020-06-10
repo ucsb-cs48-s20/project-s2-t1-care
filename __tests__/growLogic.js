e472f8b4eb094bbd42a671e58f7005eb24454243
Julian Wong <julianwong@ucsb.edu>
2020-06-09 21:51:38 -0700

    fixed testing

--- __tests__/growLogic.js
+++ __tests__/growLogic.js
@@ -3,27 +3,27 @@ import { plantGrowth } from "../utils/growLogic";
 describe("utils/growLogic", () => {
   describe("plantGrown", () => {
     it("turns 8 hours of goal and happy mood and 8 hours of sleep to 6 growth", () => {
-      expect(plantGrowth(8, "happy", 10)).toBe(6);
+      expect(plantGrowth("8 hour(s)", "happy", "10+ hour(s)")).toBe(6);
     });
 
     it("turns 6 hours of goal and okay mood and 3 hours of sleep to 1 growth", () => {
-      expect(plantGrowth(6, "okay", 2)).toBe(1);
+      expect(plantGrowth("6 hour(s)", "okay", "2 hour(s)")).toBe(1);
     });
 
     it("turns 7 hours of goal and angry mood and 10 hours of sleep into 3 growth", () => {
-      expect(plantGrowth(7, "angry", 4)).toBe(3);
+      expect(plantGrowth("7 hour(s)", "angry", "4 hour(s)")).toBe(3);
     });
 
     it("empty for type mood", () => {
       expect(() => {
-        plantGrowth(8, "", 8);
+        plantGrowth("8 hour(s)", "", "8 hour(s)");
       }).toThrow("mood should not be empty");
     });
 
     it("incorrect type for sleep", () => {
       expect(() => {
-        plantGrowth(8, "sad", "3");
-      }).toThrow("sleep should be of type int");
+        plantGrowth("8 hour(s)", "sad", 3);
+      }).toThrow("sleep should be of type string");
     });
   });
 });
